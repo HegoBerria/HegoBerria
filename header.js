8a3f27c40ec4fc656cd234bf2f605c7a2cd17ffe
GT <guillaume.tritsch.03@gmail.com>
2022-05-11 21:44:21 +0200

    Nouveau Slider

--- header.js
+++ header.js
@@ -1,8 +1,21 @@
+window.addEventListener('resize', resetSlider);
+
 function changeSliderDisplay() {
     slider = document.getElementById("background-slider");
-    if (slider.style.top == "0px") {
-        slider.style.top = "calc(-100% + 50px)";
+    if (window.matchMedia("(max-width: 600px)").matches) {
+
+        if (slider.style.top == "0px") {
+            slider.style.top = "calc(-100% + 60px)";
+        } else {
+            slider.style.top = "0px";
+        }
     } else {
+        slider.style.top = "0px";
+    }
+}
+
+function resetSlider() {
+    if (window.matchMedia("(min-width: 600px)").matches) {
         slider.style.top = "0px";
     }
 }
