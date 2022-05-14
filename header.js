912352563229cf7f13f4c5cb5e8b6f1f5fb9e35f
GT <guillaume.tritsch.03@gmail.com>
2022-05-14 13:39:37 +0200

    nouveau slider pour tel

--- header.js
+++ header.js
@@ -5,9 +5,13 @@ function changeSliderDisplay() {
     if (window.matchMedia("(orientation: portrait)").matches) {
 
         if (slider.style.top == "0px") {
+            slider.style = " background-color: rgba(0, 0, 0, 0.8) ";
             slider.style.top = "-80%";
+            
         } else {
+            slider.style = " background-color: rgba(0, 0, 0, 0) ";
             slider.style.top = "0px";
+            
         }
     } else {
         slider.style.top = "0px";
