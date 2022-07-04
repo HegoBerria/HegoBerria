017e16f470f1cd0a5200de1ad0cab78b3c42001c
GT <guillaume.tritsch.03@gmail.com>
2022-07-04 13:40:54 +0200

    Ajout footer tout support
    des modifs restent à faire

--- header.js
+++ header.js
@@ -21,3 +21,31 @@ function generateGr() {
       console.log(data)
     })
 }
+
+function copyMailInClipboard() {
+    navigator.clipboard.writeText("hegoberria64@gmail.com");
+
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode('Adresse mail copiée');
+
+
+    newDiv.style = "padding-bottom: 10px; padding-top: 10px; white-space: nowrap; color: white; border-radius: 10px; overflow: hidden; transition: all 1s; font-weight: 500; font-family: 'Roboto', sans-serif, monospace; font-size: 15px; z-index: 10; width: 0px; top: calc(50% - 150px); left: calc(50% - 150px); position: fixed; background-color: black; text-align: center;";
+
+    newDiv.appendChild(newContent);
+
+    var currentDiv = document.getElementById('page-content');
+    document.body.insertBefore(newDiv, currentDiv);
+
+    setTimeout(() => {
+        newDiv.style.width = "300px";
+    }, 0);
+
+    setTimeout(() => {
+        newDiv.style.width = "0px";
+    }, 2000);
+
+    setTimeout(() => {
+        newDiv.remove();
+    }, 3000);
+    
+}
